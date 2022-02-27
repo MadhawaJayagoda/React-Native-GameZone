86b3d0b39a0ca93c44fec6153f1fe54ae1752f99
Madhawa Jayagoda <madhawajayagoda@Madhawas-Mac-mini.local>
2022-02-27 10:26:26 +0530

    Async task added

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import * as Font from "expo-font";
+import Home from "./screens/home.js";
+import AppLoading from "expo-app-loading";
+import Navigator from './routes/drawer.js';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const getFonts = async() => {
+  console.log("Async function started");
+  await Font.loadAsync({
+    "lobster-regular": require("./assets/fonts/Lobster-Regular.ttf"),
+  });
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  const [loaded, setLoaded] = useState(false);
+  return (
+    <AppLoading
+      startAsync={getFonts}
+      onFinish={() => setLoaded(true)}
+      onError={(error)=> console.warn(error)}
+    />
+  );
+}
