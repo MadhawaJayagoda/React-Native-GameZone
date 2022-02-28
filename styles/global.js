41fe43f46532d0c05b403ff006d80de4b40070e1
Madhawa Jayagoda <madhawajayagoda@Madhawas-Mac-mini.local>
2022-02-28 09:58:41 +0530

    Ratings image source added as a global access

--- styles/global.js
+++ styles/global.js
@@ -29,3 +29,11 @@ export const globalStyles = StyleSheet.create({
         marginTop: 0
     }
 });
+
+export const ratings = {
+    1: require('../assets/rating-1.png'),
+    2: require('../assets/rating-2.png'),
+    3: require('../assets/rating-3.png'),
+    4: require('../assets/rating-4.png'),
+    5: require('../assets/rating-5.png'),
+}
